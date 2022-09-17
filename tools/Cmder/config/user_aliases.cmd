dea750f7c1423aa8e9416935f320cb11be9dde3e
Fullchee Zhang <fullchee@gmail.com>
2022-09-17 00:06:25 -0400

    Add bashrc

--- tools/Cmder/config/user_aliases.cmd
+++ tools/Cmder/config/user_aliases.cmd
@@ -9,21 +9,19 @@ gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
 pwd=cd
 clear=cls
-history=cat -n "%CMDER_ROOT%\config\.history"
 unalias=alias /d $1
 vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 
-REM ------- GIT --------
+;= REM ------- GIT --------
 config=git --git-dir=C:\.cfg --work-tree=C:\ $*
 o.=start .
 push=git commit -m "$*" && git pull && git push
 pushconfig=git --git-dir=C:\.cfg --work-tree=C:\ commit -m "$*" && git --git-dir=C:\.cfg --work-tree=C:\ pull && git --git-dir=C:\.cfg --work-tree=C:\ push
 delete_current_branch=for /f "usebackq tokens=*" %a in (`git rev-parse --abbrev-ref HEAD`) do git checkout master && git branch -D %a && git pull
 
-REM -------    --------
+;= REM -------    --------
 
 vsc=code
 youtubemp4=yt-dlp
 youtubemp3=yt-dlp --extract-audio --audio-format mp3 $1
-cat=bat
